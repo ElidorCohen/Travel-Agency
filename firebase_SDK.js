3999a13ee21b96469dfc524348dc040b4528818a
Etamar <etamar1122@gmail.com>
2022-12-27 23:01:55 +0200

    firebase

---
+++ firebase_SDK.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBojUZIODcDKQ_R3CQ0pWg-dGfd2kdk_BU",
+  authDomain: "travel-agency-949ff.firebaseapp.com",
+  projectId: "travel-agency-949ff",
+  storageBucket: "travel-agency-949ff.appspot.com",
+  messagingSenderId: "311273759394",
+  appId: "1:311273759394:web:1a097b668b349531b82dfb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
